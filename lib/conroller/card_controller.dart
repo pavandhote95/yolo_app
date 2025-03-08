513ee7f71a7561482478a86bbeec586eae10b84b
pavan <pavandhote95@gmail.com>
2025-03-08 16:50:51 +0530

    final

--- lib/conroller/card_controller.dart
+++ lib/conroller/card_controller.dart
@@ -1,6 +1,6 @@
-
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
@@ -18,12 +18,22 @@ class CardController extends GetxController {
   }
 
   Future<void> fetchCardDetails() async {
-    final response = await http.get(Uri.parse("http://localhost:3000/card"));
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      cardNumber.value = data["number"];
-      expiry.value = data["expiry"];
-      cvv.value = data["cvv"];
+    final url = kIsWeb
+        ? Uri.parse("http://localhost:3000/card")
+        : Uri.parse("http://192.168.76.224:3000/card");
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        cardNumber.value = data["number"];
+        expiry.value = data["expiry"];
+        cvv.value = data["cvv"];
+      } else {
+        print("Error: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Failed to fetch card details: $e");
     }
   }
 }
